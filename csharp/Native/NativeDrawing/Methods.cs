65bfddb830a39aa7f2c9671ae11eb26b71b4dad8
Blake O'Hare <mspaintmaestro@gmail.com>
2018-07-17 16:27:38 +0900

    scripts/generate_stubs.py now generates the C# code

--- csharp/Native/NativeDrawing/Methods.cs
+++ csharp/Native/NativeDrawing/Methods.cs
@@ -5,7 +5,7 @@ namespace MPlex.Native.Drawing
 {
     internal static class Methods
     {
-        internal static int CreateBitmapPath(Dictionary<int, object> data, string path)
+        internal static int CreateBitmapFile(Dictionary<int, object> nativeData, string path)
         {
             if (!System.IO.File.Exists(path)) return Public.Bridge.SetError(1, "File does not exist.");
             System.Drawing.Bitmap bmp;
@@ -17,32 +17,32 @@ namespace MPlex.Native.Drawing
             {
                 return Public.Bridge.SetError(1, "Invalid image file.");
             }
-            data[Public.BITMAP_BMP] = bmp;
-            data[Public.BITMAP_WIDTH] = bmp.Width;
-            data[Public.BITMAP_HEIGHT] = bmp.Height;
-            data[Public.BITMAP_PIXELS] = null;
+            nativeData[Public.BITMAP_BMP] = bmp;
+            nativeData[Public.BITMAP_WIDTH] = bmp.Width;
+            nativeData[Public.BITMAP_HEIGHT] = bmp.Height;
+            nativeData[Public.BITMAP_PIXELS] = null;
 
             return 0;
         }
 
-        internal static int CreateBitmapSize(Dictionary<int, object> data, int width, int height)
+        internal static int CreateBitmapSize(Dictionary<int, object> nativeData, int width, int height)
         {
             if (width < 1 || height < 1 || width > 5000 || height > 5000)
                 return Public.Bridge.SetError(1, "Image size is invalid.");
 
             System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(width, height);
 
-            data[Public.BITMAP_BMP] = bmp;
-            data[Public.BITMAP_WIDTH] = width;
-            data[Public.BITMAP_HEIGHT] = height;
-            data[Public.BITMAP_PIXELS] = null;
+            nativeData[Public.BITMAP_BMP] = bmp;
+            nativeData[Public.BITMAP_WIDTH] = width;
+            nativeData[Public.BITMAP_HEIGHT] = height;
+            nativeData[Public.BITMAP_PIXELS] = null;
 
             return 0;
         }
 
-        internal static int SaveBitmap(Dictionary<int, object> data, string path)
+        internal static int SaveBitmap(Dictionary<int, object> nativeData, string path)
         {
-            System.Drawing.Bitmap bmp = (System.Drawing.Bitmap)data[Public.BITMAP_BMP];
+            System.Drawing.Bitmap bmp = (System.Drawing.Bitmap)nativeData[Public.BITMAP_BMP];
             try
             {
                 bmp.Save(path);
