4be0ff2f28a76d60304f970ce1536c18690b0a61
Blake O'Hare <mspaintmaestro@gmail.com>
2018-07-27 22:25:02 +0900

    Stub out a C# harness project for building/testing libraries.

---
+++ output/CSharp/Harness/MPlexHarness/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MPlexHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ output/CSharp/Harness/MPlexHarness/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MPlexHarness")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MPlexHarness")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("04caae7b-f384-4f01-a3ca-9d03f0371239")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
